0cc7b740eb9ba36106568470378ce3c1e9d106d7
okita <t-okita@netoffice.obisan.co.jp>
2022-04-12 16:35:13 +0900

    init

---
+++ mgyk_forms/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using mgyk_forms.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace mgyk_forms
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
