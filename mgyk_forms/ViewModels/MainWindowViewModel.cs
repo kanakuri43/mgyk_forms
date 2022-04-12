14b435fc0c7d1fa801b74dc51b0e36fa13bd431e
okita <t-okita@netoffice.obisan.co.jp>
2022-04-12 16:47:21 +0900

    Dashboard作成中

--- mgyk_forms/ViewModels/MainWindowViewModel.cs
+++ mgyk_forms/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,14 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using mgyk_forms.Views;
 
 namespace mgyk_forms.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        private readonly IRegionManager _regionManager;
+
         private string _title = "Prism Application";
         public string Title
         {
@@ -11,9 +16,18 @@ namespace mgyk_forms.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        public MainWindowViewModel()
+        public MainWindowViewModel(IRegionManager regionManager)
         {
+            _regionManager = regionManager;
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(Dashboard));
 
+            LogoutCommand = new DelegateCommand(LogoutCommandExecute);
+
+        }
+        public DelegateCommand LogoutCommand { get; }
+        private void LogoutCommandExecute()
+        {
+            _regionManager.RequestNavigate("ContentRegion", nameof(Dashboard));
         }
     }
 }
